fa52fc60c8175e9f5a620b400d62a2f3805d45ea
Titaninette <letoffe.laetitia@gmail.com>
2020-11-09 10:56:41 +0100

    Button on Modules Screen to go to Comm

--- src/screens/ModuleScreen.hx
+++ src/screens/ModuleScreen.hx
@@ -3,11 +3,26 @@ package screens;
 class ModuleScreen extends dn.Process {
 	public static var ME : ModuleScreen;
 
+	var goToCommBtn : ui.Button;
+
 	public function new() {
 		super(Game.ME);
 
 		ME = this;
 
 		createRoot();
+
+		goToCommBtn = new ui.Button("Comm", Game.ME.showComm);
+		root.add(goToCommBtn, 1);
+
+		onResize();
+	}
+
+	override function onResize() {
+		super.onResize();
+
+		root.setScale(Const.SCALE);
+
+		goToCommBtn.setPosition((w() / Const.SCALE) - goToCommBtn.wid - 7, ((h() / Const.SCALE) - goToCommBtn.hei) / 2);
 	}
 }
